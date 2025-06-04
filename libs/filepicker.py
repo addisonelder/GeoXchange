bd268a5ee0e461359b1f114d45edaeac53bb2085
addisonelder <addisonelder@tuta.com>
2025-06-04 18:37:29 -0400

    fix: fixed pick_file() function

--- libs/filepicker.py
+++ libs/filepicker.py
@@ -13,6 +13,9 @@ def pick_file(extension, description, directory=None):
 
     if dialog.ShowDialog(ui.RhinoEtoApp.MainWindow) == forms.DialogResult.Ok:
         selected_files = dialog.Filenames
-        return selected_files
+        # Process the selected files
+        for file_path in selected_files:
+            print(f"Loading File: {file_path}")
+        return file_path
     else:
         raise Exception("ERROR: Invalid file, or no file was selected")
